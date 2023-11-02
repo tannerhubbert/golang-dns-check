52042063012828c3f9eb89fd07ddb459a61d44d7
Tanner Hubbert <tannerhubbert@users.noreply.github.com>
2023-11-01 21:40:19 -0500

    Update main.go

--- main.go
+++ main.go
@@ -9,9 +9,9 @@ import (
 func main() {
 	var dig dnsutil.Dig
 	dig.SetDNS("8.8.8.8")
-	var name string
+	var domain string
 	fmt.Println("What domain would you like to look up?")
-	fmt.Scanf("%s", &name)
-	a, err := dig.A(name)
+	fmt.Scanf("%s", &domain)
+	a, err := dig.A(domain)
 	fmt.Println(a, err)
 }
